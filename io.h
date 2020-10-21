0c280a94483a6073f4890cc2c3c1da2971a344b1
Javinator9889 <dev@javinator9889.com>
2020-10-21 19:19:14 +0200

    Updated license

--- io.h
+++ io.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2020 - present | pArm-S2 by Javinator9889
+ * 2020 | pArm-S2 by Javinator9889
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
